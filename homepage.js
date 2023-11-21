db4bb7a8bc6daacbb55d72a6e4fbe4e3d80d7502
WhyFenceCode <120431739+WhyFenceCode@users.noreply.github.com>
2023-11-20 19:24:38 -0800

    Update homepage.js

--- homepage.js
+++ homepage.js
@@ -1,14 +1,3 @@
-document.getElementById('url-input-homepage').addEventListener('submit', function(event) {
-    event.preventDefault(); // stop the form from submitting
-    let urliniput = document.getElementById('url-innerurl-homepage').value;
-
-    window.location.href = "https://www.google.com/search?q=" + replaceSpaces2(urliniput, "+");
-});
-
-function replaceSpaces2(inputString, char) {
-    return inputString.split(' ').join(char);
-}
-
 function changehomepagetheme(homepagetheme){
     try {
         document.getElementById('theme_css').href = "themes/" + homepagetheme;
