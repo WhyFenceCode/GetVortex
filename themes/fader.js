fbc3cf1314c1c1827b07cf29aef4d688427cddb9
WhyFenceCode <120431739+WhyFenceCode@users.noreply.github.com>
2023-11-21 15:17:33 -0800

    Update fader.js

--- themes/fader.js
+++ themes/fader.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
     $(window).scroll( function(){
     
         /* Check the location of each desired element */
-        $('.hideme').each( function(i){
+        $('.scrollopacity').each( function(i){
             
             var bottom_of_object = $(this).position().top + $(this).outerHeight();
             var bottom_of_window = $(window).scrollTop() + $(window).height();
@@ -12,7 +12,7 @@ $(document).ready(function() {
             /* If the object is completely visible in the window, remove the class */
             if( bottom_of_window > (bottom_of_object - 10)){
                 
-                $(this).removeClass('hideme');
+                $(this).removeClass('hide');
                     
             } else {
                 /* If the object is not completely visible in the window, add the 'hide' class */
