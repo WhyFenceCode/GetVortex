7d17b1197910865677606f0448d9c6ee7ee14628
WhyFenceCode <120431739+WhyFenceCode@users.noreply.github.com>
2023-11-21 17:19:55 -0800

    Update fader.js

--- fader.js
+++ fader.js
@@ -12,7 +12,7 @@ $(document).ready(function() {
             var top_of_window = $(window).scrollTop();
             
             /* If more than 10% of the object is visible in the window, remove the class */
-            if( bottom_of_window > top_of_object + 0.1*$(this).outerHeight() && top_of_window < bottom_of_object - 0.1*$(this).outerHeight()){
+            if( bottom_of_window > top_of_object + 0.5*$(this).outerHeight() && top_of_window < bottom_of_object - 0.5*$(this).outerHeight()){
                 
                 $(this).removeClass('hide');
                     
