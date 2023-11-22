46a8edfec9aad5c43c9e1cf86d8cfb13e3d76880
WhyFenceCode <120431739+WhyFenceCode@users.noreply.github.com>
2023-11-21 17:06:59 -0800

    Update fader.js

--- themes/fader.js
+++ themes/fader.js
@@ -6,16 +6,18 @@ $(document).ready(function() {
         /* Check the location of each desired element */
         $('.scrollopacity').each( function(i){
             
-            var bottom_of_object = $(this).position().top + $(this).outerHeight();
+            var top_of_object = $(this).position().top;
+            var bottom_of_object = top_of_object + $(this).outerHeight();
             var bottom_of_window = $(window).scrollTop() + $(window).height();
+            var top_of_window = $(window).scrollTop();
             
-            /* If the object is completely visible in the window, remove the class */
-            if( bottom_of_window > (bottom_of_object - 10)){
+            /* If more than 10% of the object is visible in the window, remove the class */
+            if( bottom_of_window > top_of_object + 0.1*$(this).outerHeight() && top_of_window < bottom_of_object - 0.1*$(this).outerHeight()){
                 
                 $(this).removeClass('hide');
                     
             } else {
-                /* If the object is not completely visible in the window, add the 'hide' class */
+                /* If less than 10% of the object is visible in the window, add the 'hide' class */
                 $(this).addClass('hide');
             }
             
